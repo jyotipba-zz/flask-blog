94d5a0851f862f5b7b507113e6f05e2975a7a92e
Jyoti <jyoti.bartaula@gmail.com>
2019-03-09 10:34:52 +0200

    fix spacing and styling

--- my_blog/models.py
+++ my_blog/models.py
@@ -2,16 +2,13 @@ from flask import current_app
 from my_blog import db, login
 from datetime import datetime
 from flask_login import UserMixin
-
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-
-
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-class User(UserMixin, db.Model, ):
+class User( db.Model,UserMixin ):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -19,8 +16,6 @@ class User(UserMixin, db.Model, ):
     image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     comment = db.relationship('Comment', backref='commentator', lazy='dynamic')
-
-
     def get_reset_token(self, expires_sec=1800):  # IN how many sec, token expires
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('UTF-8')  # creates token, user_id is payload
@@ -38,16 +33,15 @@ class User(UserMixin, db.Model, ):
         return 'User {}'.format(self.username)
 
 
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text,nullable = False )
     title = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
     def __repr__(self):
         return 'Post {}'.format(self.body)
+
 
 class Comment(db.Model):
     c_id = db.Column(db.Integer, primary_key=True)
@@ -55,6 +49,5 @@ class Comment(db.Model):
     c_timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     c_author_id = db.Column(db.Integer,db.ForeignKey('user.id'))
     comment_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-
     def __repr__(self):
         return 'Comment{}'.format(self.comment_id)
